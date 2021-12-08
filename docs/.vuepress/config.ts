9ebd5422a68e242783452006fcbaec2084f6bd09
cnhjp <1304272317@qq.com>
2021-12-08 23:56:49 +0800

    init commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,23 @@
+import type { ViteBundlerOptions } from '@vuepress/bundler-vite'
+import { defineUserConfig } from '@vuepress/cli'
+import type { DefaultThemeOptions } from '@vuepress/theme-default'
+
+export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
+    lang: 'en-US',
+    title: 'More',
+    description: 'An easy blog by cnhjp',
+
+    bundler: '@vuepress/bundler-vite',
+
+    themeConfig: {
+        logo: 'logo.svg',
+        repo: 'https://github.com/cnhjp/cnhjp.github.io'
+    },
+
+    plugins: ['@vuepress/search'],
+
+    head: [
+        ['link', { rel: 'icon', href: '/logo.svg' }],
+    ]
+
+})
